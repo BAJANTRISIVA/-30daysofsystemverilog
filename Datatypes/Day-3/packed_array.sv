9d752be05c09b33ccefc828367730afa485bd0c7
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 23:11:32 +0530

    Update packed_array.sv

--- Datatypes/Day-3/packed_array.sv
+++ Datatypes/Day-3/packed_array.sv
@@ -1,1 +1,25 @@
+//2d packed array
+module d2_packed_array;
+    bit [4:0][7:0]data;
+    initial begin
+        data=8'b0000_1111;
+        $display("data=%0d",data);
+        for (int i=0;i<=8;i++) begin
+            data[i]=i+1;
+            $display("data[%0d]=%b",i,data[i],data[i]);
+        end
+    end
+endmodule
 
+//single dimentional array
+
+/*module single_d;
+    bit [7:0]data;
+    initial begin
+        data=8'b1111_0010;
+        $display("data=%b",data);
+        for (int i=0;i<=7;i++) begin
+            $display("data[%0d] =%0d",i,data[i]);
+            end
+    end
+endmodule*/
