e08cc6def1dcc2337a81cb6d106a9c19d1536543
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 13:43:58 +0530

    Update Enumarated_dtype.sv

--- Datatypes/Day-1/Enumarated_dtype.sv
+++ Datatypes/Day-1/Enumarated_dtype.sv
@@ -1,1 +1,18 @@
-
+module enumarated_data_type;
+  enum {naresh,gani,hari,varun,allen,lalith,raju} name;
+  initial begin
+    $display("No.of names =%0d",name.num());
+    name=name.first();
+    $display("First name [%s] and value [%0d]",name.name(),name);
+    name=name.last();
+    $display("Last name [%s] and value [%0d]",name.name(),name);
+    name=name.next();
+    $display("Next name [%s] and value [%0d]",name.name(),name);
+    name=name.next(2);
+    $display("Next-N name [%s] and value [%0d]",name.name(),name);
+    name=name.prev();
+    $display("Previous name [%s] and value [%0d]",name.name(),name);
+    name=name.prev(2);
+    $display("Previous-N name [%s] and value [%0d]",name.name(),name);
+  end
+endmodule
