d2193b8910274645ed3556cb5feda54f8eff8414
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 22:56:54 +0530

    Update even_odd_array.sv

--- Datatypes/Day-3/even_odd_array.sv
+++ Datatypes/Day-3/even_odd_array.sv
@@ -1,1 +1,19 @@
+module even_odd_array;
+  int even[];
+  int odd[];
+  initial begin
+    even=new[10];
+    for(int i=0;i<=10;i++)
+      begin
+        even[i]=i*2;
+      end
+      $display("Even location =%p",even);
 
+    odd=new[10];
+    for(int j=1;j<=10;j++)
+      begin
+        odd[j]=j*2-1;
+      end 
+       $display("Odd location =%p",odd);
+  end
+endmodule
