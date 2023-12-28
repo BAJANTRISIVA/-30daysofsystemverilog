d80aa70022be69d0646f721a6380d10cfae98c2f
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 23:18:30 +0530

    Update unpacked_array.sv

--- Datatypes/Day-3/unpacked_array.sv
+++ Datatypes/Day-3/unpacked_array.sv
@@ -1,1 +1,24 @@
-
+//single dimentional unpacked array
+module single_unpacked;
+    byte a[5];
+    initial begin
+        foreach (a[i]) begin
+            a[i]=$random;
+            $display("i=%0d to a[i]=%0d",i,a[i]);
+        end
+        $display("a=%p",a);
+    end
+endmodule
+///multi-dimentional unpacked array
+/*module multi_dim;
+    byte a[2][4]; //2-rows,4-col
+    initial begin
+        foreach(a[i])
+        foreach(a[i][j])
+            begin
+                a[i][j]=$random;
+                $display("a[%0d][%0d] = %0d",i,j,a[i][j]);
+             end
+             $display("a=%p",a);
+    end
+endmodule*/
