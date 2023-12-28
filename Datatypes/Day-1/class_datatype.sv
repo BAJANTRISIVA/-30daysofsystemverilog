0bf0447f5918ee732df58e2f7a86b0d2c74aefb5
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 13:03:49 +0530

    Update class_datatype.sv

--- Datatypes/Day-1/class_datatype.sv
+++ Datatypes/Day-1/class_datatype.sv
@@ -1,1 +1,27 @@
+class packet;
+  ///class properties///
+  bit a;
+  string b;
+  int c;
+  byte d;
+  logic e;
+  function void display();
+    $display("a=%0d,b=%s,c=%0d,d=%0d,e=%0d",a,b,c,d,e);
+  endfunction
+endclass
 
+module above;
+  packet p1,p2;
+  initial begin
+    p1=new();
+    p2=new();
+    p1.a=1;
+    p1.b="sai";
+    p1.c=24;
+    p1.d=5;
+    p1.e=1;
+    p2.b="siva";
+    p1.display();
+    $display("p1=%p,p2=%p",p1,p2);
+  end
+endmodule
