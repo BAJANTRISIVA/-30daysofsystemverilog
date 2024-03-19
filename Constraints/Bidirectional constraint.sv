6b1cf13bd17b8c920e5015d0152a43c7030c9cd5
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2024-03-19 15:16:49 +0530

    Update Bidirectional constraint.sv

--- Constraints/Bidirectional constraint.sv
+++ Constraints/Bidirectional constraint.sv
@@ -1,1 +1,22 @@
+class bidirectional_constraint;
+  rand bit [3:0]a;
+  rand bit [4:0]b;
+  rand bit [5:0]c;
 
+  constraint a_value {a==c+b;}
+  constraint b_value {a>b;}
+  constraint c_value {c<a;}
+  function void display();
+    $display(" a=%d | b=%0d | c=%0d ",a,b,c);
+  endfunction
+endclass
+module top;
+  bidirectional_constraint bd;
+  initial begin
+    bd=new();
+    repeat(10) begin
+    bd.randomize();
+    bd.display();
+  end
+  end
+endmodule
